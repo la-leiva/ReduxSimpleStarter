c04024e435d4b5c9777ba67c868d1abcf3693f3f
Luis Alexis Leiva Díaz <luis.leiva@sngular.team>
2018-03-23 21:14:24 +0100

    Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDFC5mb8_UehfCITwC-HCdGv_W3ATDKiSM';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+YTSearch({key: API_KEY, term: 'polymer'}, function(data) {
+	console.log(data);
+})
+
+const App = () => {
+	return (<div>
+		<SearchBar />
+	</div>);
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
